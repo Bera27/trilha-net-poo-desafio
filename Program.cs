52750fb1312e6f99d766df6afdad15c4df043326
Gabriel Bera <g.bera2710@gmail.com>
2025-01-14 20:44:06 -0300

    Descrição das mudanças realizadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "125411", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Clash royale");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "55644", modelo: "Modelo 2", imei: "22221", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Clash of Clans");
